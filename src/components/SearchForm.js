da1bc4aacf4148e325c3e597152816840961a353
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-26 13:31:10 -0600

    Axios call for search is now working. Just need to display results now

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,26 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Form, Button } from "semantic-ui-react";
 import axios from "axios";
 
-export default function SearchForm({ onSearch }) {
-  const [query, setQuery] = useState({
-    name: ""
-  });
+export default function SearchForm() {
+  const [query, setQuery] = useState('');
+  const [searchFor, setSearchFor] = useState('Rick');
+
   const handleInputChange = event => {
-    setQuery({ ...query, name: event.target.value });
-    console.log('query', query);
+    setQuery(event.target.value);
   };
-  // const OnSearch = (query) => {
-    
-  // }
-  // useEffect(()=>
-  //   axios
-  //     .get(`https://rickandmortyapi.com/api/character/?name=${query.name}`)
-  //     .then(data => {
-  //       console.log("search results", data.data.results);
-  //       const results = data.data.results
-  //       return results
-  //     }),
-  //   [query]);
+  console.log('query', query);
+
+  const onSearch = (query) => {
+    // setName(query.name);
+    setSearchFor(query)
+    console.log('searching for:', searchFor);
+  }
+
+  useEffect(()=> {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?name=${searchFor}`)
+      .then(data => {
+        console.log("search results", data.data.results);
+        // const results = data.data.results
+        // return results
+      })
+  },[searchFor]);
 
   
 
@@ -30,12 +34,12 @@ export default function SearchForm({ onSearch }) {
       <Form
         className="search-form"
         size="large"
-        // onSubmit={() => OnSearch(query)}
+        onSubmit={() => onSearch(query)}
       >
         <input
           onChange={handleInputChange}
           placeholder="Enter search query"
-          value={query.search}
+          value={query}
           name="search"
         />
         <Button style={{ margin: "0px 5px" }} size="large" type="submit">
