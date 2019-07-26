54d5b48c9e0632d92fe4fde12294adf2fc78a8b1
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-26 12:01:53 -0600

    Console logging input as form is changed, but submit does not work yet. Some issue with useEffect and axios call

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,24 +1,46 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import { Form, Button } from "semantic-ui-react";
+import axios from "axios";
 
 export default function SearchForm({ onSearch }) {
   const [query, setQuery] = useState({
     name: ""
-  })
-  const handleInputChange = (event) => {
-    setQuery({ ...query, name: event.target.value })
-  }
+  });
+  const handleInputChange = event => {
+    setQuery({ ...query, name: event.target.value });
+    console.log('query', query);
+  };
+  // const OnSearch = (query) => {
+    
+  // }
+  // useEffect(()=>
+  //   axios
+  //     .get(`https://rickandmortyapi.com/api/character/?name=${query.name}`)
+  //     .then(data => {
+  //       console.log("search results", data.data.results);
+  //       const results = data.data.results
+  //       return results
+  //     }),
+  //   [query]);
+
+  
 
   return (
     <section>
-      <Form className="search-form" size='medium' onSubmit={() => onSearch(query)}>
+      <Form
+        className="search-form"
+        size="large"
+        // onSubmit={() => OnSearch(query)}
+      >
         <input
           onChange={handleInputChange}
-          placeholder="Enter your search query"
+          placeholder="Enter search query"
           value={query.search}
           name="search"
         />
-        <Button style={{margin:"0px 5px"}} size='medium' type="submit">Search</Button>
+        <Button style={{ margin: "0px 5px" }} size="large" type="submit">
+          Search
+        </Button>
       </Form>
     </section>
   );
