1def880f7e1af45d17a528eb51fe35b0e458b7d1
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-26 10:12:27 -0600

    Added axios calls with endpoints, displaying character, location, and episode names for testing

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
-
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+    //  - `https://rickandmortyapi.com/api/character/` - [`docs`](https://rickandmortyapi.com/documentation/#get-all-characters)
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then((data)=>{
+        console.log(data);
+        setCharacters(data.data.results);
+      }
+      )
+    }, [])
+    
+    console.log(characters);
+    return (
+    <section className='character-list grid-view'>
+      {characters.map((character, index)=>{
+        return (
+          <h1>{character.name}</h1>
+        )})
+      }
+          {/* <h2>TODO: `array.map()` over your state here!</h2> */}
     </section>
+    )
+  
 
 }
--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function LocationsList() {
-    const [locations, setLocations] = useState([])
+    const [episodes, setEpisodes] = useState([])
 
     useEffect(() => {
       // TODO: Add AJAX/API Request here - must run in `useEffect`
       //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
       //  - `https://rickandmortyapi.com/api/character/` - [`docs`](https://rickandmortyapi.com/documentation/#get-all-characters)
-      axios.get('https://rickandmortyapi.com/api/location/')
+      axios.get('https://rickandmortyapi.com/api/episode/')
         .then((data)=>{
-          console.log('location data',data);
-          setLocations(data.data.results);
+          console.log('episode data',data);
+          setEpisodes(data.data.results);
         }
         )
       }, [])
 
 
     return (
-        locations.map((location, index)=>{
-            return <h1>{location.name}</h1>
+        episodes.map((episode, index)=>{
+            return <h1>{episode.name}</h1>
         }
     ))
 
